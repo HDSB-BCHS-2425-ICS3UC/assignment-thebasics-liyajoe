8ac12c32b2ebe34f9bd53094465cb52db1b11bed
liyajoe <1zhouliy@hdsb.ca>
2025-02-25 13:50:47 -0500

    Comments were added
    Comments for all the blocks of code described the codes' purpose.

--- typework.py
+++ typework.py
@@ -2,19 +2,21 @@
 #Date Modified: February 24, 2025
 #Description: Different types of data types
 
-#
+#These variables store examples of the data types
 integer = 9
 float = 2.5
 string = "cookie"
 boolean = True
 char = 'g'
 
+#These variables have the definitions of the different data types
 integer_meaning = "a positive or negative whole number"
 float_meaning = "a number with a decimal"
 string_meaning = "a combination of characters"
 boolean_meaning = "true or false"
 char_meaning = "a single character"
 
+#These are print statements that tell the user about a data type and give an example
 print(f"Here all the different types of data types in Python:\n")
 print(f"We're gonna start with an integer. An integer is {integer_meaning}, such as '{integer}'.")
 print(f"A float is {float_meaning}, like '{float}'.")
