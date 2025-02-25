20cadc77d3fdbf048b4dc4db171985f1bab015ab
liyajoe <1zhouliy@hdsb.ca>
2025-02-25 14:31:30 -0500

    Variables and Print Statements Made
    Math operations were added to the variables, and then the variables were printed through print statements.

---
+++ mathoperations.py
@@ -0,0 +1,28 @@
+"""
+Perform Each math operations you have been shown (store the result in a variable) and print the result to the screen.
+
+Write a comment above each variable describing what the math operation does
+"""
+
+# Author: Liya Zhou
+# Date Modified: February 25, 2025
+# Description: 
+
+import math
+
+addition = 2+17
+subtraction = 16-5
+multiplication = 6*4
+division = 8/2
+modulus = 20%6
+exponent = 2**4
+square_root = math.sqrt(9)
+
+print(f"Here are some different math operations in Python!\n")
+print(f"Addition is 2+17, and that equals {addition}!")
+print(f"Subtraction is 16-5, and that equals {subtraction}!")
+print(f"Multiplication is 6x4, and that equals {multiplication}!")
+print(f"Division is 8/2, and that equals {division}!")
+print(f"Modulus is 20%6 (this is basically the remainder!), and that equals {modulus}!")
+print(f"An exponent is 2^4, and that equals {exponent}!")
+print(f"A square root is √9, and that equals {square_root}!")
