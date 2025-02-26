3ac005ed9499749cdbe49405e29c22aed4f31759
liyajoe <1zhouliy@hdsb.ca>
2025-02-26 13:47:27 -0500

    Added Comments to Explain Operations
    All the comments were made, and also the different math operations were explained (in comments).

--- mathoperations.py
+++ mathoperations.py
@@ -1,23 +1,20 @@
-"""
-Perform Each math operations you have been shown (store the result in a variable) and print the result to the screen.
-
-Write a comment above each variable describing what the math operation does
-"""
-
 # Author: Liya Zhou
 # Date Modified: February 25, 2025
 # Description: 
 
+#Letting Python know I want to use functions from their math library
 import math
 
-addition = 2+17
-subtraction = 16-5
-multiplication = 6*4
-division = 8/2
-modulus = 20%6
-exponent = 2**4
-square_root = math.sqrt(9)
+#Defining variables and adding math functions to them
+addition = 2+17 #Finds the sum of the provided numbers
+subtraction = 16-5 #Finds the difference of the provided numbers
+multiplication = 6*4 #Finds the product of the provided numbers
+division = 8/2 #Finds the quotient of the provided numbers
+modulus = 20%6 #Finds the remainder after dividing the provided numbers
+exponent = 2**4 #Multiplies the first number by itself (second number) times
+square_root = math.sqrt(9) #Finds what number you would have to multiply twice to obtain the inputed number
 
+#Prints all variables to the screen while explaining what math operations are used
 print(f"Here are some different math operations in Python!\n")
 print(f"Addition is 2+17, and that equals {addition}!")
 print(f"Subtraction is 16-5, and that equals {subtraction}!")
