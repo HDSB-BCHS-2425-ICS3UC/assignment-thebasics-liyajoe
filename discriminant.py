c52fb1ba2069411cc40071d6cb3c3ed5781c537b
liyajoe <1zhouliy@hdsb.ca>
2025-02-26 14:28:04 -0500

    New File Created
    A file for the discriminant task was created, which has the authoring.

---
+++ discriminant.py
@@ -0,0 +1,9 @@
+"""
+Write a program that finds the Discriminant ---- Delta = b^2 – 4ac The output should be
+
+similar to the following ( D ifficulty Mark 3 ):
+"""
+
+# Author: Liya Zhou
+# Date Modified: February 26, 2025
+# Description: Finds the discriminant (the number of solutions in a quadratic)
