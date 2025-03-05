ea495de774c2ec688c5cc776a6cd949563a7b4a7
liyajoe <1zhouliy@hdsb.ca>
2025-03-05 14:35:52 -0500

    Sleep function deleted
    Code will not be delayed anymore while running.

--- discriminant.py
+++ discriminant.py
@@ -2,22 +2,13 @@
 # Date Modified: February 27, 2025
 # Description: Finds the discriminant (the number of solutions in a quadratic)
 
-#Letting the program know I want to use a function from their "time" library
-import time
-
 #Telling the user what the code does for UX
 print("Here is code that will find the discriminant of a quadratic function...")
 
-#Having the code pause for 2 seconds to let the user register what's happening
-time.sleep(1)
-
 #Storing the user's input in variables with one second time increments in between
 a = float(input("What is your 'a' value?\nType here: "))
-time.sleep(0.5)
 b = float(input("Cool! What is your 'b' value?\nType here:"))
-time.sleep(0.5)
 c = float(input("Lastly, what is your 'c' value?\nType here:"))
-time.sleep(0.5)
 
 #Calculating the discriminant from the inputed values
 discriminant = b**2 - 4*a*c
